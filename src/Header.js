de8ff74e217f4edfbbe4e1ac436b64c521d31112
Bohdan Trynka <bohdan.trynka@perfectial.com>
2021-04-24 20:17:45 +0300

    Fixed bugs, Fixed Media, Fixed architecture

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.scss';
+import { Link } from 'react-router-dom';
+
 
 class Header extends React.Component {
     render() {
         return(
             <div className="pf-main">
                 <header className="header pf-container">
-                    <h4 className="logo">pixetic <span>design agency</span> </h4>
+                    <Link to={'/'} className="link"><h4 className="logo">pixetic <span>design agency</span> </h4></Link>
                 </header>
             </div>
         );
