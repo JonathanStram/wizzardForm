4a63b5206085808fa4f906fae8a6322ac2b89911
Bohdan Trynka <bohdan.trynka@perfectial.com>
2021-04-24 20:10:56 +0300

    Test commit

---
+++ src/Terms.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Terms extends React.Component {
+    constructor(props){
+        super(props);
+}
+
+
+
+    render () {
+        return(
+            <h1>This is Terms page</h1>
+        );        
+    }
+}
+
+export default Terms
